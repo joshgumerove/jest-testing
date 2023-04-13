26ddb2ebe65b9ee74cd87dff398c2de28a81c95c
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-13 10:55:28 -0400

    strings and floats

--- sum.test.js
+++ sum.test.js
@@ -13,3 +13,31 @@ describe("example tests", () => {
     expect(obj).toEqual({});
   });
 });
+
+describe("truthy or falsy", () => {
+  it("null", () => {
+    expect(null).toBeFalsy();
+    expect(null).not.toBeTruthy();
+    expect(null).toBeNull();
+  });
+});
+
+describe("numbers", () => {
+  it("two plus two", () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeLessThan(5);
+  });
+
+  it("adding floats", () => {
+    const value = 0.1 + 0.2;
+    expect(value).toBeCloseTo(0.29999);
+  });
+});
+
+describe("strings", () => {
+  it("checks for the inclusion of Josh Gumerove", () => {
+    const value = "Josh Gumerove is so cool";
+    expect(value).toMatch(/Josh Gumerove/);
+  });
+});
